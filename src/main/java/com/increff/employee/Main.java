f7679912abd24e9c18afb833faacec307bd2d742
DESKTOP-TOG7637\DELL <sendtonamratha@gmail.com>
2022-05-24 18:08:16 +0530

    changes commited

--- src/main/java/com/increff/employee/Main.java
+++ src/main/java/com/increff/employee/Main.java
@@ -9,16 +9,14 @@ import java.sql.Statement;
 public class Main {
 	public static void main(String[] args) throws SQLException, ClassNotFoundException {
 		Class.forName("com.mysql.cj.jdbc.Driver");  
-		Connection con=DriverManager.getConnection(  
-		"jdbc:mysql://localhost:3306/pos","increff","password1234"); 
+		Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/pos","project_increff","password1234"); 
 		
 		Statement stmt =con.createStatement();
-		ResultSet rs=stmt.executeQuery("select * from pos");
+		ResultSet rs=stmt.executeQuery("select * from brand_details");
 		while(rs.next()) {
 			System.out.println(rs.getInt(1)+" "+ rs.getString(2)+" "+ rs.getString(3));
 		}
 		con.close();
-		
 		
 		
 	}
